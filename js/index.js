81d2f164a195b6cfb2528d21a6e395fa64493f75
Assolita <118940874+Assolita@users.noreply.github.com>
2023-01-15 22:04:41 -0300

    projeto finalizado
    funcionalidade de efetuar reserva através do modal adicionada, assim como a de excluir reserva, e página de exibir reservas adicionada

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 const divEventos = document.querySelector("#eventos")
 
-const getEvents = async() => {    
+const getEvents = async(eventos) => {    
     const events = await fetch(`${BASE_URL}/events`).then((result) => 
         result.json()
         )
+
+        events.forEach((event) => {
+            const ids = event._id
+            console.log(ids)
+        })
 
     events
     .sort((eventoA, eventoB) => eventoA.scheduled - eventoB.scheduled)
@@ -15,10 +20,105 @@ const getEvents = async() => {
             <h2>${event.name} - ${new Date (event.scheduled).toLocaleDateString('pt-BR')}</h2>
             <h4>${event.attractions.join(", ")}</h4>
             <p>${event.description}</p>
-            <a href="#" class="btn btn-primary">reservar ingresso</a>
+            <button  id="reserva" class="btn btn-primary">reservar ingresso</a>
         </article>`
         divEventos.appendChild(article)
     });
+
+    // events.forEach((event) => {
+    //     const ids = event._id
+    //     console.log(ids)
+    // })
+
+
+
+    // const idd = document.getElementsByClassName('idd');
+    // // for (let idx, i = 0; i < idd.length; i++) {
+    // //     idx = idd[i];
+    // //     idx.push(idd);
+    // //     }
+
+
+
+const closeModal = document.querySelector("#close-modal")
+const bodyModal = document.querySelector("#modal")
+const fade = document.querySelector("#fade")
+
+const toggleModal = () => {
+    [bodyModal, fade].forEach((element) => element.classList.toggle("hide"))
 }
+
+
+    // if (reserva.length) {
+    //     for (var i = 0; i < reserva.length; i++) {
+    //         reserva[i].addEventListener("click", toggleModal());
+    //     }
+    // }
+
+
+    // const reservas = []
+    // const reserva = document.getElementsByClassName('btn btn-primary')
+    // reservas.push(reserva)
+
+    // reservas.forEach((a) => {
+    //     a.addEventListener("click", () => toggleModal())})
+
+    const reservas = document.getElementsByClassName('btn btn-primary');
+    for (let reserva, i = 0; i < reservas.length; i++) {
+        reserva = reservas[i];
+        reserva.addEventListener('click', toggleModal);
+        }
+
+
+
+closeModal.addEventListener("click", () => toggleModal()) 
+fade.addEventListener("click", () => toggleModal()) 
+
+// [closeModal, fade].forEach((elemento) => {
+//     elemento.addEventListener("click", () => toggleModal())
+// })
+
+
+const emailUsuario = document.querySelector("#email")
+const nomeUsuario = document.querySelector("#nome")
+const formModal = document.querySelector("#formModal")
+
+formModal.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+        
+
+    const bodym = { // criando objeto que será enviado no POST pra API
+        owner_name: nomeUsuario.value,
+        owner_email: emailUsuario.value,
+        number_tickets: "1",
+        event_id: "63c2fee5364d291a5f4609c5"
+    }
+    console.log(bodym)
+    const response = fetch(`${BASE_URL}/bookings`, { // requisição de envio para o banco de dados
+        method: "POST", 
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(bodym)
+    }).then((response) => {
+        if (response.ok){
+            alert("Reserva efetuada com sucesso!")
+            window.location.replace("index.html") // volta a página
+        }else {
+            alert("Falha ao efetuar reserva :(")
+        }
+    })
+
+
+
+})
+
+const idReserva = document.querySelector("#idReserva")
+idReserva.textContent = 'fsafasas'
+
+}
+
+
+divEventos.appendChild(modal)
 
 getEvents()
